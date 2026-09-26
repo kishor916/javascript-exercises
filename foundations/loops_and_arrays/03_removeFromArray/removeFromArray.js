1af9cf9dd0f58a48732e7229232c85211a135b6d
kishor shrestha <kisshor916@gmail.com>
2026-09-26 21:47:45 +1000

    03 finished

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  // Use filter to build a new array with items that are NOT in args
+  const newArray = array.filter((item) => !args.includes(item));
+
+  return newArray;
 };
 
 // Do not edit below this line
